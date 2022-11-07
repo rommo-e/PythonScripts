0f5a8b5546c2f6fa6e1424b959f079adcbb41259
rommo-e <rommo@ciencias.unam.mx>
2022-11-07 15:40:47 -0600

    Scripts loops

---
+++ Basic programs/foreachloop.py
@@ -0,0 +1,11 @@
+physics_branchs = [
+	'QED' #index 0
+	'Electromagnetism'#index1
+	'Termodynamics'#index 2
+	'Quantum'#index 3
+	'Astronomy'#index 4
+	'Statistics'#index5
+]
+
+for branch in physics_branchs:
+	print(f'{branch }\n, is a physical science.')
